99e2e0c83c7c0b105096f2aa33e19c42ccbf15b2
Anahita Sabaghi <anahita.rastegar@gmail.com>
2024-08-15 18:03:23 +0330

    I cloned a project from https://github.com/Aalem ’s #GitHub and upgraded the #UI with #AppBar, #SafeArea, #BoxDecoration, and #TextStyle.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        appBar: AppBar(
+          title: const Text('My App'),
+          backgroundColor: Colors.teal[700],
+        ),
+        backgroundColor: Colors.teal[100],
+        body: SafeArea(
+          child: Center(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              height: 100,
+              width: 200,
+              margin: EdgeInsets.all(20),
+              padding: EdgeInsets.all(20),
+              child: Center(
+                child: Text(
+                  'Hello World!',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.teal[900],
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
